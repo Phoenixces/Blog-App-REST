18ef581533c8c1fa76e6071f97dff46ea34d0c8f
Phoenixces <karunasharma21032002@gmail.com>
2024-12-01 10:49:23 +0530

    Added code for SuperAdmin Insertion

--- src/main/java/com/springboot/blog/utils/PasswordGeneratorEncoder.java
+++ src/main/java/com/springboot/blog/utils/PasswordGeneratorEncoder.java
@@ -6,6 +6,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class PasswordGeneratorEncoder {
     public static void main(String[] args) {
         PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        System.out.println(passwordEncoder.encode("admin"));
+        String adminPassword="Admin123";
+        System.out.println(passwordEncoder.encode(adminPassword));
     }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO myblog.roles (id, name) VALUES (1, 'ROLE_ADMIN'), (2, 'ROLE_USER')
+ON DUPLICATE KEY UPDATE name=VALUES(name);
